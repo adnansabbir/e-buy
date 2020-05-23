a419384fe5c6977c33dbc3ed302de435962e3b16
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-05-24 05:47:36 +0600

    Updated app with shop page and collection preview component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
-
 import Homepage from "./pages/homepage/homepage.page";
 import {Route} from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return (
         <div>
             <Route exact={true} path='/' component={Homepage}/>
+            <Route exact={true} path='/shop' component={ShopPage}/>
         </div>
     );
 }
