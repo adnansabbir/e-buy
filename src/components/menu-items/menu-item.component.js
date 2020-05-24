5fa3565f6429367ed6b369a838a3b050cacc0bac
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-05-24 06:44:38 +0600

    Added grid layout for better UI

--- src/components/menu-items/menu-item.component.js
+++ src/components/menu-items/menu-item.component.js
@@ -4,7 +4,7 @@ import {withRouter} from 'react-router-dom'
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
     return (
-        <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
+        <div className={`${size === 'large' ? 'large' : 'small'} menu-item`} onClick={() => history.push(linkUrl)}>
             <div
                 className="background-image"
                 style={{backgroundImage: `url(${imageUrl})`}}
