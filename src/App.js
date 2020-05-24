7b93355a8c045cfc39be4071a0cdbe217d4630ec
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-05-25 05:37:55 +0600

    Added added sign in component layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from "./pages/homepage/homepage.page";
 import {Route} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Header/>
             <Route exact={true} path='/' component={Homepage}/>
             <Route exact={true} path='/shop' component={ShopPage}/>
+            <Route exact={true} path='/signin' component={SignInAndSignUp}/>
         </div>
     );
 }
