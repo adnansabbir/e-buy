1dbb3c03e14d0538b3adfdf30dee8ca3c38d9368
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-05-30 17:49:44 +0600

    Added signin feature for custom email

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -2,7 +2,7 @@ import React from "react";
 import './sign-in.style.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoogle} from "../../firebase/firebase.utils";
+import {auth, signInWithGoogle} from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
     constructor() {
@@ -14,9 +14,15 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({email: '', password: ''});
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     handleChange = event => {
@@ -48,7 +54,8 @@ class SignIn extends React.Component {
 
                     <div className="buttons">
                         <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} extraClassNames='google-sign-in'>Sign In With Google</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} extraClassNames='google-sign-in'>Sign In With
+                            Google</CustomButton>
                     </div>
                 </form>
             </div>
