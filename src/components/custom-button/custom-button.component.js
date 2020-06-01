483b70456a7172c920a94e7b7f022d485ed1994d
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-06-01 22:08:36 +0600

    Added Add button to Collection - item

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -2,7 +2,10 @@ import React from "react";
 import './custom-button.style.scss';
 
 const CustomButton = ({children, extraClassNames, ...otherProps}) => (
-    <button className={`custom-button ${extraClassNames ? extraClassNames : ''}`} {...otherProps}>{children}</button>
+    <button
+        className={`custom-button ${extraClassNames ? extraClassNames : ''}`} {...otherProps}>
+        {children}
+    </button>
 );
 
 export default CustomButton;
