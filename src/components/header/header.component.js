6a2dd5e3c14746089e6b6d35270e7fa75ae79e3c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-06-01 12:06:03 +0600

    Added cart icon and cart dropdown component with toggle functionality with reducer

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,8 +4,10 @@ import './header.style.scss';
 import {ReactComponent as Logo} from "../../assets/crow.svg";
 import {auth} from "../../firebase/firebase.utils";
 import {connect} from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../card-dropdown/card-dropdown.component";
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, cartDropDownHidden}) => (
     <div className="header">
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -18,12 +20,15 @@ const Header = ({currentUser}) => (
                     <Link className='option' to={'/'} onClick={() => auth.signOut()}>Sign Out</Link>
                     : <Link className='option' to={'/signin'}>Sign In</Link>
             }
+            <CartIcon/>
         </div>
+        {cartDropDownHidden ? null : <CartDropDown/>}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser: currentUser,
+    cartDropDownHidden: hidden
 });
 
 export default connect(mapStateToProps)(Header);
