18d4a0e576d54390980f249d9b5cdf320c80bc4d
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-06-04 16:51:13 +0600

    removed redux-persist-filter library

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import {createStore, applyMiddleware} from "redux";
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from "redux-persist";
 
-const middleware = [logger];
+const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store)
+
+export {store, persistor};
