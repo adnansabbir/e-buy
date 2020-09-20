45374c0828acc31f88a19f5e00f088a2b18418af
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-09-20 09:15:55 +0600

    Added memoization to cart item

--- client/src/components/cart-item/cart-item.component.js
+++ client/src/components/cart-item/cart-item.component.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {memo} from "react";
 import './cart-item.style.scss';
 
-const CartItem = ({item: {imageUrl, price, name, quantity}}) => (
-    <div className="cart-item">
-        <img src={imageUrl} alt={name}/>
-        <div className="item-details">
-            <span className="name">{name}</span>
-            <span className="price">{quantity} x ${price}</span>
-        </div>
-    </div>
-);
+class CartItem extends React.Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return JSON.stringify(this.props.item) !== JSON.stringify(nextProps.item);
+    }
 
-export default CartItem
+    render() {
+        const {item: {imageUrl, price, name, quantity}} = this.props;
+        return (
+            <div className="cart-item">
+                <img src={imageUrl} alt={name}/>
+                <div className="item-details">
+                    <span className="name">{name}</span>
+                    <span className="price">{quantity} x ${price}</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CartItem;
