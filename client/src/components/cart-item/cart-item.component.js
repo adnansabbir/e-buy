7dd2beb65a0e9741f17ff0b1d3490f78fe6a8412
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-09-26 03:55:16 +0600

    Added react memo to optimize rendering

--- client/src/components/cart-item/cart-item.component.js
+++ client/src/components/cart-item/cart-item.component.js
@@ -1,23 +1,16 @@
 import React, {memo} from "react";
 import './cart-item.style.scss';
 
-class CartItem extends React.Component {
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return JSON.stringify(this.props.item) !== JSON.stringify(nextProps.item);
-    }
-
-    render() {
-        const {item: {imageUrl, price, name, quantity}} = this.props;
-        return (
-            <div className="cart-item">
-                <img src={imageUrl} alt={name}/>
-                <div className="item-details">
-                    <span className="name">{name}</span>
-                    <span className="price">{quantity} x ${price}</span>
-                </div>
+const CartItem = ({item: {imageUrl, price, name, quantity}}) => (
+    (
+        <div className="cart-item">
+            <img src={imageUrl} alt={name}/>
+            <div className="item-details">
+                <span className="name">{name}</span>
+                <span className="price">{quantity} x ${price}</span>
             </div>
-        )
-    }
-}
+        </div>
+    )
+);
 
-export default CartItem;
+export default React.memo(CartItem);
